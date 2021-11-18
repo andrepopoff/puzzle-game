c499f57df4cb7d50274446af70e60ee62f047374
Andrei Popov <andrepopoff@gmail.com>
2021-11-18 21:15:21 +0300

    Show camera on the page

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-function main() {
-  console.log('main')
+let video = null;
+let canvas = null;
+let context = null;
+
+async function main() {
+  try {
+    canvas = document.getElementById('game');
+    context = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const signal = await navigator.mediaDevices.getUserMedia({video: true});
+    video = document.createElement('video');
+    video.srcObject = signal;
+    video.play();
+
+    video.onloadeddata = function () {
+      updateCanvas();
+    }
+  } catch (error) {
+    alert('Camera error: ' + error);
+  }
+}
+
+function updateCanvas() {
+  context.drawImage(video, 0, 0);
+  window.requestAnimationFrame(updateCanvas);
 }
